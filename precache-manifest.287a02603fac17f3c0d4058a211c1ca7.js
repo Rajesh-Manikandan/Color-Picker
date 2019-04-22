1013876716f58f81efaf841b382e0e164ab5de95
Rajesh-Manikandan <welearntogether.rj@gmail.com>
2019-04-22 12:14:05 +0530

    Updates

---
+++ precache-manifest.287a02603fac17f3c0d4058a211c1ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6db800b3f2cb2e1a4df6",
+    "url": "/Color-Picker/static/js/runtime~main.7bd9117e.js"
+  },
+  {
+    "revision": "8e2b41476934a305905f",
+    "url": "/Color-Picker/static/js/main.5589562e.chunk.js"
+  },
+  {
+    "revision": "f7f4030b396ed859f944",
+    "url": "/Color-Picker/static/js/2.4af14f31.chunk.js"
+  },
+  {
+    "revision": "8e2b41476934a305905f",
+    "url": "/Color-Picker/static/css/main.cf3d3b66.chunk.css"
+  },
+  {
+    "revision": "f7f4030b396ed859f944",
+    "url": "/Color-Picker/static/css/2.f8027876.chunk.css"
+  },
+  {
+    "revision": "49b389802a32413ef9791d6513477a50",
+    "url": "/Color-Picker/index.html"
+  }
+];
